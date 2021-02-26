ea19049210e7b7cffbf215473a0e5a8a8d645fe4
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-26 11:04:31 +0200

    Adding Cart_Item

---
+++ src/CartItem/CartItem.tsx
@@ -0,0 +1,11 @@
+import Button from '@material-ui/core/Button'
+
+// Types
+import {CartItemType} from './CartItem.styles';
+
+// Styles
+import {Wrapper} from './CartItem.styles';
+
+const CartItem: React.FC = () => <div>Cart Item</div>;
+
+export default CartItem;
